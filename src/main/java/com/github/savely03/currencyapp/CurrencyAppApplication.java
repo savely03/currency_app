9a3a8c132a4eaaeeeb0bf879e6f1661aa858c6e2
Savely Domnikov <savely.domnikov@yandex.ru>
2023-07-25 12:25:53 +0300

    Initial commit

---
+++ src/main/java/com/github/savely03/currencyapp/CurrencyAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.savely03.currencyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/savely03/currencyapp/CurrencyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.savely03.currencyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
